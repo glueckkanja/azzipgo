343332c1d211bf5159c0dfc7169b6a817784c47b
Jan Berdel <jan.berdel@glueckkanja.com>
2018-06-13 21:43:34 +0200

    Fix typo in "deploy-in-place"

--- AzZipGo/DeployInplaceOptions.cs
+++ AzZipGo/DeployInplaceOptions.cs
@@ -4,7 +4,7 @@ namespace AzZipGo
 {
     public class DeployInplaceOptions : DeployOptions
     {
-        public override string CommandName => "deploy-inplace";
+        public override string CommandName => "deploy-in-place";
         public override string CommandHelp => "Deploy Site using ZipDeploy and run the deployment in-place without a temporary slot and auto-swap.";
     }
 }
--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 * Runs **everywhere**
 * Uses Kudu's **ZipDeploy** feature
 * Uses an auto-generated **Deployment Slot and Auto-Swap** when using the _deploy-with-slot_ command
-* Or deploys **directly to a WebSite** slot using the _deploy-inplace_ command
+* Or deploys **directly to a WebSite** slot using the _deploy-in-place_ command
