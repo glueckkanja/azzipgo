94ed70da7c5107882a6198abd34e2d65db73dff8
Jan Berdel <jan.berdel@glueckkanja.com>
2019-02-20 13:48:37 +0100

    Add support for --run-from-package when using in-place deployment

--- AzZipGo/DeployInplace.cs
+++ AzZipGo/DeployInplace.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Azure.Management.AppService.Fluent;
+using Microsoft.Azure.Management.AppService.Fluent.Models;
+using System;
 using System.IO;
 using System.Net;
 using System.Threading.Tasks;
@@ -14,6 +16,9 @@ namespace AzZipGo
         public override async Task<int> RunAsync()
         {
             var app = await GetSiteAsync();
+
+            await ManageRunFromZipAsync();
+
             var ppTarget = await GetTargetPublishingProfileAsync(app);
 
             var latestDeployment = await GetLatestDeployment(ppTarget);
@@ -33,6 +38,80 @@ namespace AzZipGo
             Console.WriteLine(success ? "Deployment succeeded." : "Deployment failed.");
 
             return success ? 0 : 100;
+        }
+
+        private async Task ManageRunFromZipAsync()
+        {
+            using (var mgmtClient = new WebSiteManagementClient(RestClient) { SubscriptionId = Options.Subscription })
+            {
+                Task<SlotConfigNamesResourceInner> slotNamesTask;
+                Task<StringDictionaryInner> settingsTask;
+
+                if (Options.TargetSlot != "production")
+                {
+                    slotNamesTask = mgmtClient.WebApps.ListSlotConfigurationNamesAsync(Options.ResourceGroup, Options.Site);
+                    settingsTask = mgmtClient.WebApps.ListApplicationSettingsSlotAsync(Options.ResourceGroup, Options.Site, Options.TargetSlot);
+                }
+                else
+                {
+                    slotNamesTask = mgmtClient.WebApps.ListSlotConfigurationNamesAsync(Options.ResourceGroup, Options.Site);
+                    settingsTask = mgmtClient.WebApps.ListApplicationSettingsAsync(Options.ResourceGroup, Options.Site);
+                }
+
+                var slotNames = await slotNamesTask;
+                var settings = await settingsTask;
+
+                if (Options.TargetSlot != "production")
+                {
+                    if (Options.RunFromPackage)
+                    {
+                        await MakeRunFromPackageNonStickyIfRequiredAsync(mgmtClient, slotNames);
+
+                        if (settings.Properties.TryGetValue("WEBSITE_RUN_FROM_PACKAGE", out var value) && value != "1")
+                        {
+                            settings.Properties["WEBSITE_RUN_FROM_PACKAGE"] = "1";
+                            await mgmtClient.WebApps.UpdateApplicationSettingsSlotAsync(Options.ResourceGroup, Options.Site, settings, Options.TargetSlot);
+                            Console.WriteLine("Set application setting WEBSITE_RUN_FROM_PACKAGE to 1.");
+                        }
+                    }
+                    else if (settings.Properties.TryGetValue("WEBSITE_RUN_FROM_PACKAGE", out var value))
+                    {
+                        settings.Properties.Remove("WEBSITE_RUN_FROM_PACKAGE");
+                        await mgmtClient.WebApps.UpdateApplicationSettingsSlotAsync(Options.ResourceGroup, Options.Site, settings, Options.TargetSlot);
+                        Console.WriteLine("Removed application setting WEBSITE_RUN_FROM_PACKAGE.");
+                    }
+                }
+                else
+                {
+                    if (Options.RunFromPackage)
+                    {
+                        await MakeRunFromPackageNonStickyIfRequiredAsync(mgmtClient, slotNames);
+
+                        if (settings.Properties.TryGetValue("WEBSITE_RUN_FROM_PACKAGE", out var value) && value != "1")
+                        {
+                            settings.Properties["WEBSITE_RUN_FROM_PACKAGE"] = "1";
+                            await mgmtClient.WebApps.UpdateApplicationSettingsAsync(Options.ResourceGroup, Options.Site, settings);
+                            Console.WriteLine("Set application setting WEBSITE_RUN_FROM_PACKAGE to 1.");
+                        }
+                    }
+                    else if (settings.Properties.TryGetValue("WEBSITE_RUN_FROM_PACKAGE", out var value))
+                    {
+                        settings.Properties.Remove("WEBSITE_RUN_FROM_PACKAGE");
+                        await mgmtClient.WebApps.UpdateApplicationSettingsAsync(Options.ResourceGroup, Options.Site, settings);
+                        Console.WriteLine("Removed application setting WEBSITE_RUN_FROM_PACKAGE.");
+                    }
+                }
+            }
+        }
+
+        private async Task MakeRunFromPackageNonStickyIfRequiredAsync(WebSiteManagementClient mgmtClient, SlotConfigNamesResourceInner slotNames)
+        {
+            if (!slotNames.AppSettingNames.Contains("WEBSITE_RUN_FROM_PACKAGE"))
+                return;
+
+            slotNames.AppSettingNames.Remove("WEBSITE_RUN_FROM_PACKAGE");
+            await mgmtClient.WebApps.UpdateSlotConfigurationNamesAsync(Options.ResourceGroup, Options.Site, slotNames);
+            Console.WriteLine("Removed application setting WEBSITE_RUN_FROM_PACKAGE from list of slot settings.");
         }
     }
 }
--- AzZipGo/DeployInplaceOptions.cs
+++ AzZipGo/DeployInplaceOptions.cs
@@ -4,7 +4,14 @@ namespace AzZipGo
 {
     public class DeployInplaceOptions : DeployOptions
     {
+        public DeployInplaceOptions()
+        {
+            Command.Options.Add("run-from-package", "Set or remove sticky app setting WEBSITE_RUN_FROM_PACKAGE for the target slot. This will enable or disable the Run From Package feature. Default = false", s => RunFromPackage = (s != null));
+        }
+
         public override string CommandName => "deploy-in-place";
         public override string CommandHelp => "Deploy Site using ZipDeploy and run the deployment in-place without a temporary slot and auto-swap.";
+
+        public bool RunFromPackage { get; set; }
     }
 }
