2e645f33ead97e484cd0acef36c34a352a008980
Jan Berdel <jan.berdel@glueckkanja.com>
2020-02-06 11:15:40 +0100

    Trying to fix push cmd

--- .deploy.sh
+++ .deploy.sh
@@ -1,3 +1,3 @@
 cd "AzZipGo/bin/Release"
 FILE=$(ls -t azzipgo.*.nupkg | head -1)
-dotnet nuget push $FILE -k "$NUGET_API_KEY" -s "https://api.nuget.org/v3/index.json"
+dotnet nuget push "$FILE" -s "https://api.nuget.org/v3/index.json" -k "$NUGET_API_KEY"
